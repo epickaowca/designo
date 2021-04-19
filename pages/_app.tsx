7abbc02264390ac3938335e4f79a01cdb9ea786e
epickaowca <owcaofficial@yahoo.com>
2021-04-19 17:34:53 +0200

    button elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,39 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+import App from 'next/app'
+import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import { createWrapper } from 'next-redux-wrapper'
+import store from '../redux/store'
 import { myTheme } from '../styles/styles'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={myTheme}>
-      <Head>
+class MyApp extends App {
+  render(){
+    const { Component, pageProps } = this.props
+    return (
+      <Provider store={store}>
+        <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
             name="description"
-            content="Designo, portfolio"
+            content="invoice-app"
           />    
           <meta name="author" content="MP" />
-          <meta name="keywords" content="designo, designo portfolio, web developer" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap" rel="stylesheet" />
+          <meta name="keywords" content="invoice-app, app for invoice" />
           <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <ThemeProvider theme={myTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    )
+  }
 }
 
-export default MyApp
+const makestore = ()=>store
+const wrapper = createWrapper(makestore)
+
+export default wrapper.withRedux(MyApp)
--- styles/styles.ts
+++ styles/styles.ts
@@ -6,11 +6,19 @@ media: {
     desktop: "@media only screen and (min-width: 1440px)",
 },
 colors: {
-    peach: '#e7816b',
-    light_peach: '#ffad9b',
-    dark_grey: '#333136',
-    light_grey: '#f1f3f5',
-    black: '#1d1c1e',
+    blue: '#7C5DFA',
+    medium_blue: '#9277FF',
+    very_dark_blue: '#1E2139',
+    dark_slate_blue: '#252945',
+    lavender: '#DFE3FA',
+    light_steel_blue: '#888EB0',
+    cornflower_blue: '#7E88C3',
+    black: '#0C0E16',
+    tomato: '#EC5757',
+    light_salmon: '#9277FF',
+    light: '#F8F8FB',
+    midnight: '#141625',
+    white_lavender: '#F9FAFE'
 },
 };
 
